6163d737f3d931a34a325e0c0e33ef190ccd54dd
PipeBarros <felipebarroy@gmail.com>
2025-03-31 19:25:45 +0000

    Primera versión del código

--- src/app.py
+++ src/app.py
@@ -25,18 +25,61 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#Obtener todos los usuarios
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def all_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
+        "msg": "Users retrieved succesfully",
         "family": members
     }
 
-
     return jsonify(response_body), 200
+
+#Obtener un usuario
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_one_user(member_id):
+    member = jackson_family.get_member(member_id)
+    
+    if not member:
+        return jsonify({"msg": "Member not found"}), 404
+    response_body = {
+        "msg": "Member found succesfully",
+        "id": member['id'],
+        "first_name": member['first_name'],
+        "age": member['age'],
+        #Necesario use de list() ya que JSON no puede serializar conjuntos directamente
+        "lucky_numbers": list(member['lucky_numbers'])
+    }
+    return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_new_member():
+    request_body = request.get_json()
+    #Verificación de datos
+    if not request_body:
+        return jsonify({"msg": "Missing request body"}), 400
+    #Verifica de que los datos requeridos estén en la request_body
+    if "first_name" not in request_body or "age" not in request_body or "lucky_numbers" not in request_body:
+        return jsonify({"msg": "Missing required fields"}), 400
+    
+    try:
+        #Intenta agregar el nuevo miembro
+        new_member = jackson_family.add_member(request_body)
+        #En caso de respuesta exitosa
+        return jsonify({"msg": "Member added successfully"}), 201
+    except (TypeError, ValueError) as error:
+        return jsonify({"msg": str(error)}), 400
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    deleted_member = jackson_family.delete_member(member_id)
+    if deleted_member:
+        return jsonify({"done": True, "msg": "Member deleted successfully"}), 200
+    else:
+        return jsonify({"done": False, "msg": "Member not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -6,31 +6,81 @@ update this file to implement the following already declared methods:
 - update_member: Should update a member from the self._members list
 - get_member: Should return a member from the self._members list
 """
-from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
+        self._next_id = 1
         # example list of members
         self._members = []
+        self._default_members = [
+            {
+                "first_name": "John",
+                "age": 33,
+                "lucky_numbers": {7, 13, 22},
+                "id": self._generate_id(),
+            },
+            {
+                "first_name": "Jane",
+                "age": 35,
+                "lucky_numbers": {10, 14, 3},
+                "id": self._generate_id(),
+            },
+            {
+                "first_name": "Jimmy",
+                "age": 5,
+                "lucky_numbers": {1},
+                "id": self._generate_id(),
+            }
+        ]
+        for member in self._default_members:
+            self._members.append(member)
 
     # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
-        return randint(0, 99999999)
+    def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        #Comprueba que member sea un diccionario
+        if not isinstance(member, dict):
+            raise TypeError("The member must be a dict")
+        #Comprueba que member el lucky_number sea un conjunto(set), si no lo es, lo transforma
+        if 'lucky_numbers' in member and not isinstance(member['lucky_numbers'], set):
+            member['lucky_numbers'] = set(member['lucky_numbers'])
+        #Comprueba si 'age' sea un int mayor que 0
+        if not isinstance(member['age'], int) or member['age'] <= 0:
+            raise ValueError("Age must be a positive integer")
+        #Comprueba que exista id en member
+        if 'id' not in member:
+            member['id'] = self._generate_id()
+        #Fuerza el apellido de la clase
+        member['last_name'] = self.last_name
+        self._members.append(member)
+        return member
+        
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        #Verifica la longitud del array
+        initial_len = len(self._members)
+        #Recorre los miembros devolviendo sólo aquellos donde el 'id' es diferente al 'id' dado como argumento
+        self._members = [memb for memb in self._members if memb['id'] != id]
+        #Utilizo operador de comparación para retornar un booleano. True si la función se cumple.
+        return len(self._members) < initial_len
+
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        #Recorre los miembros dentro de self._members, retornando el miembro con el 'id' dado como argumento.
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        #Si no encuentra al miembro con el 'id' dado retorna None
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
